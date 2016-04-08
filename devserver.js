48fb7e558d51efa9dc54cae774acdaa3bd61773f
sergeyt <stodyshev@gmail.com>
2016-04-08 23:25:13 +0600

    use react-devpack

--- devserver.js
+++ devserver.js
@@ -1,40 +1,8 @@
-const path = require('path');
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
-const webpack = require('webpack');
+const dev = require('react-devpack');
 const proxyMiddleware = require('http-proxy-middleware');
-const config = require('./webpack.config');
 
-const port = 8000;
-const app = express();
-const compiler = webpack(config);
-
-app.use(morgan('dev'));
-app.use(cors());
-
-app.use(require('webpack-dev-middleware')(compiler, {
-	publicPath: config.output.publicPath,
-	stats: {
-		colors: true,
+dev.startServer({
+	extendApp(app) {
+		app.use(proxyMiddleware('http://localhost:8094/api'));
 	},
-}));
-
-app.use(require('webpack-hot-middleware')(compiler));
-
-app.use(proxyMiddleware('http://localhost:8094/api'));
-
-app.use(express.static(process.cwd()));
-
-app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.listen(port, '0.0.0.0', (err) => {
-	if (err) {
-		console.log(err);
-		return;
-	}
-
-	console.log('Listening at http://0.0.0.0:%s', port);
 });
