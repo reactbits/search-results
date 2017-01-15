4c251eb5a732f5e7442333722279b497e3bf3989
sergeyt <stodyshev@gmail.com>
2017-01-15 21:20:01 +0700

    fix eslint errors

--- src/hit.jsx
+++ src/hit.jsx
@@ -3,9 +3,8 @@ import React from 'react';
 import Fragments from './fragments';
 
 export default function Hit(props) {
-  const fragments = _.map(props.fragments, (val, key) =>
-    <Fragments key={key} fieldName={key} fragments={val} />
-  );
+  const frag = (val, key) => <Fragments key={key} fieldName={key} fragments={val} />;
+  const fragments = _.map(props.fragments, frag);
   return (
     <li>
       <b>{props.id}</b>
